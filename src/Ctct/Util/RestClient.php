1fea2668b1611b26318d06917735a0096f8351a4
djellesma <djellesma@constantcontact.com>
2013-05-21 12:01:46 -0400

    Removing debug line left in RestClient.php

--- src/Ctct/Util/RestClient.php
+++ src/Ctct/Util/RestClient.php
@@ -70,7 +70,6 @@ class RestClient implements RestClientInterface
      */
     private static function httpRequest($url, $method, array $headers = array(), $data = null)
     {
-        echo $url;
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_URL, $url);
         curl_setopt($curl, CURLOPT_HEADER, 0);

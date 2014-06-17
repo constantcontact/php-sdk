0edad80287122be6a800332e5d29024b29685d84
Ben Huebscher <huebs@me.com>
2014-06-17 14:34:23 +0300

    Revised documentation to accurately reflect returned variable type.

--- src/Ctct/Util/RestClient.php
+++ src/Ctct/Util/RestClient.php
@@ -15,7 +15,7 @@ class RestClient implements RestClientInterface
      * Make an Http GET request
      * @param $url - request url
      * @param array $headers - array of all http headers to send
-     * @return array - array of the response body, http info, and error (if one exists)
+     * @return CurlResponse - The response body, http info, and error (if one exists)
      */
     public function get($url, array $headers)
     {
@@ -27,7 +27,7 @@ class RestClient implements RestClientInterface
      * @param $url - request url
      * @param array $headers - array of all http headers to send
      * @param $data - data to send with request
-     * @return array - array of the response body, http info, and error (if one exists)
+     * @return CurlResponse - The response body, http info, and error (if one exists)
      */
     public function post($url, array $headers = array(), $data = null)
     {
@@ -39,7 +39,7 @@ class RestClient implements RestClientInterface
      * @param $url - request url
      * @param array $headers - array of all http headers to send
      * @param $data - data to send with request
-     * @return array - array of the response body, http info, and error (if one exists)
+     * @return CurlResponse - The response body, http info, and error (if one exists)
      */
     public function put($url, array $headers = array(), $data = null)
     {
@@ -50,7 +50,7 @@ class RestClient implements RestClientInterface
      * Make an Http DELETE request
      * @param $url - request url
      * @param array $headers - array of all http headers to send
-     * @return array - array of the response body, http info, and error (if one exists)
+     * @return CurlResponse - The response body, http info, and error (if one exists)
      */
     public function delete($url, array $headers = array())
     {

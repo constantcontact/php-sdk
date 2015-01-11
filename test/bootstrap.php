608108d5b9743627a457698e4670f4e4b840029c
Nick Galbraith <ngalbraith@constantcontact.com>
2015-01-11 16:55:18 -0700

    PHPUnit fixes for TravisCI - at least trying

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -6,8 +6,4 @@ require_once(realpath(dirname(__FILE__) . '/../') . "/src/Ctct/autoload.php");
 require_once(__DIR__ . "/Json/JsonLoader.php");
 
 // autoload composer dependencies
-if (!@include __DIR__ . '/../vendor/autoload.php') {
-    die('You must set up the project dependencies, run the following commands:
-        wget http://getcomposer.org/composer.phar
-        php composer.phar install');
-}
+require_once(realpath(dirname(__FILE__) . '/../') . "/vendor/autoload.php");

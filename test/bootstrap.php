630f33fa55010eeac12efe694ebeaaaa6b455459
Nick Galbraith <ngalbraith@constantcontact.com>
2015-01-11 16:31:17 -0700

    PHPUnit fixes for TravisCI

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -2,5 +2,7 @@
 // autoloader for the SDK
 require_once(realpath(dirname(__FILE__) . '/../') . "/src/Ctct/autoload.php");
 
+require_once(realpath(dirname(__FILE__) . '/../') . "/vendor/autoload.php");
+
 // load the JsonLoader
 require_once(__DIR__ . "/Json/JsonLoader.php");
